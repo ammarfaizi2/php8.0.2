0a3ede14b5e56ba1cc659549d716242ff69bc4e5
Ammar Faizi <ammarfaizi2@gmail.com>
2021-02-07 15:11:30 +0700

    Add config
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

---
+++ copy_cfg.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cp -vf my_cfg/php-cli.ini /usr/local/bin/php-cli.ini;
+cp -vf my_cfg/php-fpm-fcgi.ini /usr/local/lib/php-fpm-fcgi.ini;
+cp -vf my_cfg/php-fpm.conf /usr/local/etc/php-fpm.conf;
+cp -vf my_cfg/www.conf /usr/local/etc/php-fpm.d/www.conf;
+
+chown -v nobody:nogroup \
+	/usr/local/bin/php-cli.ini \
+	/usr/local/lib/php-fpm-fcgi.ini \
+	/usr/local/etc/php-fpm.conf \
+	/usr/local/etc/php-fpm.d/www.conf;
+
+chmod -v u=r,go= \
+	/usr/local/bin/php-cli.ini \
+	/usr/local/lib/php-fpm-fcgi.ini \
+	/usr/local/etc/php-fpm.conf \
+	/usr/local/etc/php-fpm.d/www.conf;
